b315478566c3b132126d9af9a785dbc0bb51fe21
GalS <sobol.gal@gmail.com>
2021-12-16 10:59:23 +0200

    make a oprimized version

---
+++ CALCEXrepl.cpp
@@ -0,0 +1,63 @@
+#include <iostream>
+using namespace std;
+int main(int argc, char const *argv[])
+{
+    double input1, input2, result;
+    char oprator, qmark;
+    do
+    {
+        if (qmark == '?')
+        {
+            cin >> oprator >> input1 >> input2 >> qmark;
+            switch (oprator)
+            {
+            case '+':
+                result = input1 + input2;
+                break;
+            case '-':
+                result = input1 - input2;
+                break;
+            case '*':
+                result = input1 * input2;
+                break;
+            case '/':
+                result = input1 / input2;
+                break;
+            case '>':
+                result = input2 > input2;
+                break;
+            case '<':
+                result = input2 < input2;
+                break;
+            case 'E':
+                result = input1 == input2;
+                break;
+            case 'D':
+                result = input1 != input2;
+                break;
+            case 'B':
+                result = input1 >= input2;
+                break;
+            case 'S':
+                result = input1 <= input2;
+                break;
+            case 'A':
+                result = input1 && input2;
+                break;
+            case 'O':
+                result = input1 || input2;
+                break;
+            case 'N':
+                result = !input1;
+                break;
+            default:
+                break;
+            }
+            cout<<"    >>>"<<result;
+        }
+        
+    } while (oprator == 'e');
+    
+    
+    return 0;
+}
--- main.py
+++
@@ -1,6 +0,0 @@
-import math
-print("START")
-expr = 0
-while expr != "ENDP":
-    expr = input()
-    print(f">>{expr}")
